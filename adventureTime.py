eb0334385a9018025c921130e27ef77cdad0be31
Kagemni <elsawi.karim@gmail.com>
2023-04-21 15:38:08 -0400

    Update adventureTime.py
    Altered existing code to add type annotations, Added Dungeon level to represent where the adventurer currently is, and added the character sheet

--- adventureTime.py
+++ adventureTime.py
@@ -1,11 +1,12 @@
 
 
 
-class mobs():
-    def __init__(self, name, health, attack):
+class mobs:
+    def __init__(self, name: str, health: int, attack: int, defense: int):
         self.name = name
         self.health = health
         self.attack = attack
+        self.defense = defense
 
     def damageTaken(self,damage):
         self.health -= damage
@@ -13,10 +14,11 @@ class mobs():
         if self.health <= 0:
             print (self.name, "has been slain")
 
-    def damageDealt(self,attack):
+    def damageDealt(self,target):
         print(self.name, "has attacked you for", self.attack, "damage.")
-        target.take_damage(self.attack)
+        target.take_damage(self.attack) #idk if this will work
 
+""" 
 class slime(mobs):
 
 class zombie(mobs):
@@ -25,6 +27,31 @@ class zombie(mobs):
 class ogre(mobs):
 
 
-class dragon(mobs):
+class dragon(mobs): """
 
+class dungeonLevel:
+    def __init__(self, floor: int, mobs: str):
+        self.floor = floor
+        self.mobs = mobs
+    def getMobs(self):
+        return self.mobs
+    def get_level(self):
+        return self.floor
 
+class character:
+    def __init__(self, profession: str, race: str, level: int, name: str) -> None:
+        self.profession = profession
+        self.race = race
+        self.level = level
+        self.name = name
+
+class stats:
+    def __init__(self, health: int, attack: int, defense: int) -> None:
+        self.health = health
+        self.attack = attack
+        self.defense = defense
+
+class adventurerSheet:
+    def __init__(self, character: character, stats: stats ) -> None:
+        self.character = character
+        self.stats = stats
